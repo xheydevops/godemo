8ae4d726dccdb927413de6e06b514b80936f8aa2
andyfenggq <362739259@qq.com>
2020-11-09 11:30:19 +0800

    添加test

---
+++ go_test.go
@@ -0,0 +1,16 @@
+/*
+ * @Date: 2020-11-09 11:18:42
+ * @Author: fenggq
+ * @LastEditors: fenggq
+ * @LastEditTime: 2020-11-09 11:25:10
+ * @FilePath: /godemo/go_test.go
+ */
+package main
+
+import "testing"
+
+//TestRun ...
+func TestRun(t *testing.T) {
+	t.Log("test", "TestRun")
+	t.Error("===")
+}
