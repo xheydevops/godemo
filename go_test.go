56155522f70c86d1e08737261b384a24fe54da8f
andyfenggq <362739259@qq.com>
2020-11-09 14:47:37 +0800

    测试

--- go_test.go
+++ go_test.go
@@ -2,15 +2,30 @@
  * @Date: 2020-11-09 11:18:42
  * @Author: fenggq
  * @LastEditors: fenggq
- * @LastEditTime: 2020-11-09 11:25:10
+ * @LastEditTime: 2020-11-09 14:47:15
  * @FilePath: /godemo/go_test.go
  */
 package main
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
 //TestRun ...
 func TestRun(t *testing.T) {
+	log.Printf("App Name:\t%s\n", AppName)
+	log.Printf("App Version:\t%s\n", AppVersion)
+	log.Printf("Build version:\t%s\n", BuildVersion)
+	log.Printf("Build time:\t%s\n", BuildTime)
+	log.Printf("Git revision:\t%s\n", GitRevision)
+	//log.Printf("Git branch:\t%s\n", GitBranch)
+	log.Printf("Golang Version: %s\n", GoVersion)
 	t.Log("test", "TestRun")
-	t.Error("===")
+	//t.Error("===")
+}
+
+func TestRun2(t *testing.T) {
+	t.Log("testrun2")
+	t.Error("TestRun2")
 }
