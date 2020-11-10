1111e276f7e3ac615d957b68c0a60fd14029f10b
andyfenggq <362739259@qq.com>
2020-11-10 19:41:56 +0800

    提交代码======神鼎飞丹砂的地方发撒的过去而过奥德赛干围观阿萨德刚问过安全的施工围观sad刚问过阿萨德刚问过我奥德赛干围观阿三哥富翁砂锅问公司垫付\nsadgwedddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddg

--- main.go
+++ main.go
@@ -2,12 +2,19 @@
  * @Date: 2020-11-08 19:04:14
  * @Author: fenggq
  * @LastEditors: fenggq
- * @LastEditTime: 2020-11-08 20:06:15
+ * @LastEditTime: 2020-11-10 19:39:45
  * @FilePath: /godemo/main.go
  */
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
 
 var (
 	//AppName 应用名称
@@ -26,15 +33,64 @@ var (
 	GoVersion string
 )
 
-//
+//GoTest ...
+type GoTest struct {
+	Responser      string   `json:"responser"`
+	Version        string   `json:"version"`
+	API            string   `json:"api"`
+	Cmd            string   `json:"cmd"`
+	ExpectedOutput []string `json:"expectedOutput"`
+	Valid          bool     `json:"valid"`
+}
+
+//CMD ...
+func CMD(command string) (string, error) {
+	cmd := exec.Command("/bin/sh", "-c", command)
+	data, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("cmd:%s, get error %s\n", command, err.Error())
+	}
+	return (string)(data), err
+}
 func main() {
 	log.SetFlags(log.Lshortfile)
-	log.Printf("App Name:\t%s\n", AppName)
-	log.Printf("App Version:\t%s\n", AppVersion)
-	log.Printf("Build version:\t%s\n", BuildVersion)
-	log.Printf("Build time:\t%s\n", BuildTime)
-	log.Printf("Git revision:\t%s\n", GitRevision)
-	//log.Printf("Git branch:\t%s\n", GitBranch)
-	log.Printf("Golang Version: %s\n", GoVersion)
-	log.Println("=====")
+	gitlog, err := CMD("git log --date=format:'%Y-%m-%d %H:%M:%S'")
+	//str := strings.Replace(gitlog, "\n", "", -1)
+	//log.Println(str)
+	kv := strings.Split(gitlog, "\n")
+	for k, v := range kv {
+		log.Println(k, v)
+	}
+
+	return
+	//cmd := os.Args[0]
+	name := os.Args[1]
+	errMssage := ""
+	for i, a := range os.Args[2:] {
+		fmt.Printf("Argument %d is %s\n", i+1, a)
+		if errMssage != "" {
+			errMssage += " "
+		}
+		errMssage += a
+	}
+	log.Println(name, errMssage)
+
+	msg := &GoTest{}
+	err = json.Unmarshal([]byte(errMssage), msg)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("%#v,%#v", msg.Responser, msg)
+	dingTalk := DingTalk{
+		Robot: Robot{
+			WebHook: "https://oapi.dingtalk.com/robot/send?access_token=07de3a3799f70778bb98f95e7ef64b1693b30415a3ec59ea42e97de873f1aee0",
+		},
+	}
+	//	out, err := json.Marshal(msg)
+	param := &JenkinsMessageParam{}
+	param.GitCommitName = name
+	param.ErrorMsg = errMssage
+	dingTalk.SendJenkinsMessage(param, msg)
+	//dingTalk.SendTextMessage(string(out))
+
 }
